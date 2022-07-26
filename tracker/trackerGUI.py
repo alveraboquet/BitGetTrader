9e29f865c29f3702433d146685ac0dbeeeb54c87
Brett Beaulieu <bbeaulieu1@live.com>
2022-07-26 08:15:16 -0400

    directory refactoring

--- tracker/trackerGUI.py
+++ tracker/trackerGUI.py
@@ -8,40 +8,38 @@ from dateutil import parser
 
 class TrackerGUI(QWidget):
 
-    def __init__(self):
+    def __init__(self, csvpath):
         super().__init__()
         self.setWindowTitle("Portfolio Tracker")
         self.setGeometry(0,0,1280,720)
-        self.build_chart()
+        self.build_chart(csvpath)
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.chartview)
+        self.layout.addWidget(self.tradeTable)
         self.setLayout(self.layout)
         self.show()
 
 
-    def build_chart(self):
-
-
-
-
+    def build_chart(self, csvpath):
         self.chart = QChart()
 
+        self.build_line_portfolio(csvpath)
+        
         self.xaxis = QDateTimeAxis(self)
         self.xaxis.setTickCount(10)
         self.xaxis.setFormat("MMM dd, yyyy")
         self.xaxis.setTitleText("Date")
+        self.chart.addAxis(self.xaxis, Qt.AlignBottom)
+        self.portfolio_series.attachAxis(self.xaxis)
 
         self.yaxis = QValueAxis(self)
         self.yaxis.setTickCount(11)
         self.yaxis.setLabelFormat("$%.2f")
         self.yaxis.setTitleText("Value")
         self.yaxis.setRange(0,100)
-        
-        self.chart.addAxis(self.xaxis, Qt.AlignBottom)
         self.chart.addAxis(self.yaxis, Qt.AlignLeft)
-        self.build_line_portfolio()
-        self.portfolio_series.attachAxis(self.xaxis)
         self.portfolio_series.attachAxis(self.yaxis)
+        
 
         self.chart.setAnimationOptions(QChart.SeriesAnimations)
         self.chart.setTitle("Portfolio")
@@ -62,4 +60,6 @@ class TrackerGUI(QWidget):
             epoch = parser.parse(data[0]).timestamp()*1000
             self.portfolio_series.append(epoch,data[1])
 
-        self.chart.addSeries(self.series)
+        self.chart.addSeries(self.portfolio_series)
+
+    def build_trade_table(self, csvpath:str,feesOn:bool=True, startDate:str="", endDate:str=""):
